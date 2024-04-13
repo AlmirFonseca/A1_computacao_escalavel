043eb381fa04f12daacbb160eda588da55fc59af
Luan Rodrigues de Carvalho <105639491+Luan-vht3@users.noreply.github.com>
2024-04-13 17:12:50 -0300

    Create TimerTrigger.hpp

---
+++ src/TimerTrigger.hpp
@@ -0,0 +1,55 @@
+#ifndef TIMERTRIGGER_HPP
+#define TIMERTRIGGER_HPP
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "AbstractTrigger.hpp"
+using namespace std;
+
+// TimerTrigger class
+/**
+ * @brief Trigger activated at regular intervals.
+ * 
+ * This class represents a trigger that activates at regular intervals.
+ */
+class TimerTrigger : public Trigger {
+private:
+    std::chrono::milliseconds interval;
+    bool active;
+
+public:
+    /**
+     * @brief Constructs a new TimerTrigger object with a given interval.
+     * 
+     * @param interval The interval between activations.
+     */
+    TimerTrigger(std::chrono::milliseconds interval) : interval(interval), active(false) {}
+
+    /**
+     * @brief Sets the interval between activations.
+     * 
+     * @param newInterval The new interval.
+     */
+    void setInterval(std::chrono::milliseconds newInterval) {
+        interval = newInterval;
+    }
+
+    // Overriding activate method
+    void activate() override {
+        active = true;
+        std::thread([this]() {
+            while (active) {
+                std::this_thread::sleep_for(interval);
+                std::cout << "TimerTrigger activated!" << std::endl;
+            }
+        }).detach();
+    }
+
+    // Overriding deactivate method
+    void deactivate() override {
+        active = false;
+    }
+};
+
+#endif // TIMERTRIGGER_HPP
