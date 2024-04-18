5284989b3d79d0edb59253b57b06def4cccb3be2
Gabriel Pereira <gabrielpereirajp1201@gmail.com>
2024-04-18 19:40:13 -0300

    Create semaphore class

---
+++ src/Semaphore.hpp
@@ -0,0 +1,52 @@
+# ifndef SEMAPHORE_HPP
+# define SEMAPHORE_HPP
+
+#include <mutex>
+#include <condition_variable>
+
+/**
+ * @brief Class for handling a semaphore.
+ */
+
+class Semaphore {
+private:
+    int count;
+    int max_count;
+    std::mutex mutex;
+    std::condition_variable condition;
+
+public:
+    /**
+     * @brief Construct a new Semaphore object.
+     * 
+     * @param count The initial count of the semaphore.
+     */
+    Semaphore(int count = 0, int max_count = 1) : count(count), max_count(max_count) {}
+
+    virtual ~Semaphore() = default;
+
+    /**
+     * @brief Wait for the semaphore.
+     */
+    void wait() {
+        std::unique_lock<std::mutex> lock(mutex);
+        while (count == 0) {
+            condition.wait(lock);
+        }
+        count--;
+    }
+
+    /**
+     * @brief Notify the semaphore.
+     */
+    void notify() {
+        std::unique_lock<std::mutex> lock(mutex);
+        while (count == max_count) {
+            condition.wait(lock);
+        }
+        count++;
+        condition.notify_one();
+    }
+};
+
+# endif // SEMAPHORE_HPP
