8507ad3937f65e06f51f435c670421a98793d6a6
JuCarv-bit <juliana.souza.opp@gmail.com>
2024-04-21 16:16:15 -0300

    Add script to run mock and the ETL (main)

---
+++ run_it.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run MOCK script
+echo "Running MOCK..."
+cd mock || { echo "Error: Couldn't navigate to mock directory"; exit 1; }
+python main.py & disown  # Run in background and detach
+
+cd -  # Navigate back to root directory
+
+# Run ETL script
+echo "Running ETL..."
+cd src || { echo "Error: Couldn't navigate to src directory"; exit 1; }
+g++ main.cpp -o main || { echo "Error: Compilation failed"; exit 1; }
+./main.exe
+cd -  # Navigate back to root directory
+
+echo "Done!"
